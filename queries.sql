cdda64730e8d6353727833b918cea6f31de52641
Manel <manelhammouche3@gmail.com>
2022-07-06 15:13:08 +0100

    Fix query

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 SELECT * from animals WHERE name = 'Luna';
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-12-2019';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
