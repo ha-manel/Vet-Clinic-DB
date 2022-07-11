1f4c4a7ab0ff19b3038f55b971b7fce9f9ac5ad2
EidHachem <eidhachem1@gmail.com>
2022-07-11 15:47:10 +0300

    Add explain analyze queries

--- queries.sql
+++ queries.sql
@@ -210,3 +210,9 @@ JOIN vets ON vets.id = visits.vet_id AND vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY COUNT(species_id) DESC
 LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
